5bdf62ee28b4f77849e3a2573005dfe80a48a534
Cyrus-0101 <cyrus_g@outlook.com>
2025-05-27 20:37:57 +0300

    fix: Replace platform allocation with memory management allocation in create_game()

--- testbed/src/entry.c
+++ testbed/src/entry.c
@@ -2,8 +2,7 @@
 
 #include <entry.h>
 
-// TODO: Remove this
-#include <platform/platform.h>
+#include <core/kmemory.h>
 
 /**
  * @brief Creates and configures the game instance.
@@ -31,7 +30,7 @@ b8 create_game(game* out_game) {
     out_game->on_resize = game_on_resize;
 
     // Create the game state.
-    out_game->state = platform_allocate(sizeof(game_state), FALSE);
+    out_game->state = kallocate(sizeof(game_state), MEMORY_TAG_GAME);
 
     return TRUE;
 }
