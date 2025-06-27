4cf7be98a3509435801712a9b35c29a9088ecd05
Cyrus-0101 <cyrus_g@outlook.com>
2025-06-27 12:07:15 +0300

    fix:Update max_frames_in_flight calculation for swapchain

--- engine/src/renderer/vulkan/vulkan_swapchain.c
+++ engine/src/renderer/vulkan/vulkan_swapchain.c
@@ -122,7 +122,6 @@ b8 vulkan_swapchain_acquire_next_image_index(
     VkSemaphore image_available_semaphore,
     VkFence fence,
     u32* out_image_index) {
-
     VkResult result = vkAcquireNextImageKHR(
         context->device.logical_device,
         swapchain->handle,
@@ -163,7 +162,6 @@ void vulkan_swapchain_present(
     VkQueue present_queue,
     VkSemaphore render_complete_semaphore,
     u32 present_image_index) {
-
     // Return the image to the swapchain for presentation.
     VkPresentInfoKHR present_info = {VK_STRUCTURE_TYPE_PRESENT_INFO_KHR};
     present_info.waitSemaphoreCount = 1;
@@ -201,7 +199,6 @@ void vulkan_swapchain_present(
  */
 void create(vulkan_context* context, u32 width, u32 height, vulkan_swapchain* swapchain) {
     VkExtent2D swapchain_extent = {width, height};
-    swapchain->max_frames_in_flight = 2;
 
     // Choose a swap surface format.
     b8 found = False;
@@ -252,6 +249,8 @@ void create(vulkan_context* context, u32 width, u32 height, vulkan_swapchain* sw
     if (context->device.swapchain_support.capabilities.maxImageCount > 0 && image_count > context->device.swapchain_support.capabilities.maxImageCount) {
         image_count = context->device.swapchain_support.capabilities.maxImageCount;
     }
+
+    swapchain->max_frames_in_flight = image_count - 1;
 
     // Build the swapchain creation info
     VkSwapchainCreateInfoKHR swapchain_create_info = {VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR};
