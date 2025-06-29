466b7f256fa1d067124addf6c02a7a78a750eb3b
Cyrus-0101 <cyrus_g@outlook.com>
2025-06-29 09:05:11 +0300

    refactor: Improve keypress handling in win32_process_message for better clarity and efficiency

--- engine/src/platform/platform_win32.c
+++ engine/src/platform/platform_win32.c
@@ -287,25 +287,18 @@ LRESULT CALLBACK win32_process_message(HWND hwnd, u32 msg, WPARAM w_param, LPARA
             b8 pressed = (msg == WM_KEYDOWN || msg == WM_SYSKEYDOWN);
             keys key = (u16)w_param;
 
+            b8 is_extended = (HIWORD(l_param) & KF_EXTENDED) == KF_EXTENDED;
+
+            // Keypress only determines if _any_ alt/ctrl/shift key is pressed.
             if (w_param == VK_MENU) {
-                // References: https://learn.microsoft.com/en-us/windows/win32/learnwin32/keyboard-input
-                if (GetKeyState(VK_RMENU) & 0x8000) {
-                    key = KEY_RALT;
-                } else if (GetKeyState(VK_LMENU) & 0x8000) {
-                    key = KEY_LALT;
-                }
+                key = is_extended ?  KEY_RALT : KEY_LALT;
             } else if (w_param == VK_SHIFT) {
-                if (GetKeyState(VK_RSHIFT) & 0x8000) {
-                    key = KEY_RSHIFT;
-                } else if (GetKeyState(VK_LSHIFT) & 0x8000) {
-                    key = KEY_LSHIFT;
-                }
+                // KF_EXTENDED is not set for shift keys, 
+                u32 left_shift = MapVirtualKey(VK_LSHIFT, MAPVK_VK_TO_VSC);
+                u32 scancode = ((l_param & (0xFF << 16)) >> 16);
+                key = scancode == left_shift ? KEY_LSHIFT : KEY_RSHIFT;
             } else if (w_param == VK_SHIFT) {
-                if (GetKeyState(VK_RCONTROL) & 0x8000) {
-                    key = KEY_RCONTROL;
-                } else if (GetKeyState(VK_LCONTROL) & 0x8000) {
-                    key = KEY_LCONTROL;
-                }
+                key = is_extended ? KEY_RCTRL : KEY_LCTRL;
             }
 
             // Pass to the input subsystem for processing
