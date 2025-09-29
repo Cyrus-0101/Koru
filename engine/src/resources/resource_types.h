0288b0282c810e224348e3e91e166f1e235ca099
Cyrus-0101 <cyrus_g@outlook.com>
2025-09-29 16:07:25 +0300

    feat: Add maximum length for texture names and introduce texture_use enumeration

--- engine/src/resources/resource_types.h
+++ engine/src/resources/resource_types.h
@@ -11,6 +11,9 @@
  * to how the engine represents and manipulates graphical assets.
  */
 
+/** Maximum length for texture names. */
+#define TEXTURE_NAME_MAX_LENGTH 512
+
 /**
  * @struct texture
  * @brief Represents a texture resource.
@@ -31,6 +34,20 @@ typedef struct texture {
     b8 has_transparency;
     /** Internal data pointer for backend-specific texture representation. */
     u32 generation;
+    /** Name of the texture allocated for identification purposes. */
+    char name[TEXTURE_NAME_MAX_LENGTH];
     /** Pointer to backend-specific internal data (e.g., Vulkan image and view). */
     void* internal_data;
 } texture;
+
+/**
+ * @enum texture_use
+ * @brief Enumerates the intended uses for a texture.
+ *
+ * This helps the engine understand how to apply the texture in rendering,
+ * such as whether it's a diffuse map, normal map, etc.
+ */
+typedef enum texture_use {
+    TEXTURE_USE_UNKNOWN = 0x00,
+    TEXTURE_USE_MAP_DIFFUSE = 0x01
+} texture_use;
