1621b356e9edf332fde587bfd9d6cd3203082f29
Cyrus-0101 <cyrus_g@outlook.com>
2025-10-07 21:53:07 +0300

    feat: Implement binary resource loader with load and unload functionality

---
+++ engine/src/resources/loaders/binary_loader.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "systems/resource_system.h"
+
+/**
+ * @file binary_loader.h
+ * @brief Defines the binary resource loader.
+ */
+
+/**
+ * @brief Creates a binary resource loader.
+ * @return The binary resource loader.
+ */
+resource_loader binary_resource_loader_create();
