544a6b1f449d1d875310f0059b846e7cbaee6dda
Cyrus-0101 <cyrus_g@outlook.com>
2025-10-07 21:53:41 +0300

    feat: Implement text resource loader with load and unload functionality

---
+++ engine/src/resources/loaders/text_loader.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "systems/resource_system.h"
+
+/**
+ * @file text_loader.h
+ * @brief Defines the text resource loader.
+ */
+
+/**
+ * @brief Creates a text resource loader.
+ * @return The text resource loader.
+ */
+resource_loader text_resource_loader_create();
